7dbb230976a6bedcb1fd954107457bb4178eecef
Jodi N Kansagor <joedvice@gmail.com>
2019-11-06 09:11:32 -0800

    added actual location search

--- server.js
+++ server.js
@@ -14,62 +14,60 @@ const PORT = process.env.PORT;
 // - enable CORS
 app.use(cors());
 
-let latlngs;
+// let latlngs;
 
 // Api routes
-app.get('/location', (request, response) => {
+app.get('/location', async(request, response) => {
     try {
-        const location = request.query.search;
-        const result = getLatLng(location);
-        response.status(200).json(result);
+        const searchQuery = request.query.search;
+        const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+        
+        const locationItem = await superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${GEOCODE_API_KEY}`);
+        
+        const parsedLocation = JSON.parse(locationItem.text).results[0];
+       console.log(parsedLocation)
+        const formattedSearchResult = toLocation(parsedLocation);
+        console.log(formattedSearchResult);
+
+        response.status(200).json(formattedSearchResult);
     }
     catch (err) {
         response.status(500).send('Sorry, something went wrong. Please try again');    
     }
 });
 
-app.get('/weather', (request, response) => {
-    try {
-        const weather = request.query.weather;
-        const result = formatWeather(weather);
-        response.status(200).json(result);
-    }
-    catch (err) {
-        response.status(500).send('Sorry, something went wrong. Please try again');   
-    }
-});
+// app.get('/weather', (request, response) => {
+//     try {
+//         const weather = request.query.weather;
+//         const result = formatWeather(weather);
+//         response.status(200).json(result);
+//     }
+//     catch (err) {
+//         response.status(500).send('Sorry, something went wrong. Please try again');   
+//     }
+// });
 
-const geoData = require('./data/geo.json');
-const weatherData = require('./data/darksky.json');
 
-function getLatLng(location) {
-    if (location === 'bad location') {
-        throw new Error();
-    }
-    return toLocation(geoData);
-}
+function toLocation(locationItem) {
 
-function toLocation(geoData) {
-    const firstResult = geoData.results[0];
-    const geometry = firstResult.geometry;
+    const geometry = locationItem.geometry;
 
     return {
-        formatted_query: firstResult.formatted_address,
+        formatted_query: locationItem.formatted_address,
         latitude: geometry.location.lat,
         longitude: geometry.location.lng
     };
 }
 
-function formatWeather() {
-    return weatherData.daily.data.map(day => {
-        return {
-            forecast: day.summary,
-            time: new Date(day.time * 1000).toDateString()
-        };
-    });
-}
-console.log(weatherData.daily.data);
-formatWeather(weatherData);
+// function formatWeather() {
+//     return weatherData.daily.data.map(day => {
+//         return {
+//             forecast: day.summary,
+//             time: new Date(day.time * 1000).toDateString()
+//         };
+//     });
+// }
+// formatWeather(weatherData);
 
 // Start the server
 app.listen(PORT, () => {
